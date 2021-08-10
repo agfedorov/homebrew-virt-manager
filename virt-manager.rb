b9c1efb4e0666cf52c7af0ab27aff0c3c0e8949a
Anton Fedorov <a.g.fedorov@gmail.com>
2021-08-10 16:18:38 +0400

    fix

--- virt-manager.rb
+++ virt-manager.rb
@@ -21,6 +21,7 @@ class VirtManager < Formula
   depends_on "python"
   depends_on "spice-gtk"
   depends_on "vte3"
+  depends_on "pygobject3"
 
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/6d/78/f8db8d57f520a54f0b8a438319c342c61c22759d8f9a1cd2e2180b5e5ea9/certifi-2021.5.30.tar.gz"
@@ -45,11 +46,6 @@ class VirtManager < Formula
   resource "pycairo" do
     url "https://files.pythonhosted.org/packages/bc/3f/64e6e066d163fbcf13213f9eeda0fc83376243335ea46a66cefd70d62e8f/pycairo-1.20.1.tar.gz"
     sha256 "1ee72b035b21a475e1ed648e26541b04e5d7e753d75ca79de8c583b25785531b"
-  end
-
-  resource "PyGObject" do
-    url "https://files.pythonhosted.org/packages/51/2f/4d5d5afb7000b9151e33952b59163c9389bd867ac6fe85d62f85831fa061/PyGObject-3.40.1.tar.gz"
-    sha256 "6fb599aa59ceb9dd05fafb0d72b3862943e7d5e85c8ef6c74856bc6d4321cbab"
   end
 
   resource "requests" do
